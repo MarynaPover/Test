7df4e5ee57832d672774f440ca3a45efb9eb304c
MarynaPover <marynapover@gmail.com>
2022-12-20 18:50:12 +0000

    1

--- hw3.py
+++ hw3.py
@@ -2,8 +2,8 @@ a1 = (1, 'aaa', 2)
 a2 = (1, 'aaa', 2)
 a3 = (1, 'aaa', 2)
 
-print (a1 == a2 == a3)
-print (a1 is a2 is a3)
+print(a1 == a2 == a3)
+print(a1 is a2 is a3)
 
 print(a1)
 print(a2)
@@ -18,8 +18,8 @@ print(id(a3))
 a4 = [1, 'aaa', 2]
 a5 = [1, 'aaa', 2]
 
-print (a4 == a5)
-print (a4 is a5)
+print(a4 == a5)
+print(a4 is a5)
 
 print(a4)
 print(a5)
