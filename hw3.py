897dc6f9931e3e5eef1dbbbe94b9062a179c4e89
MarynaPover <marynapover@gmail.com>
2022-12-20 18:42:34 +0000

    1

--- hw3.py
+++ hw3.py
@@ -1,6 +1,6 @@
-a1 = ....
-
-a2 = ....
+a1 = (1, 'aaa', 2)
+a2 = (1, 'aaa', 2)
+a3 = (1, 'aaa', 2)
 
 a1 == a2
 
@@ -10,11 +10,18 @@ a1 is a2
 
 True
 
+print(a1)
+print(a2)
+print(a3)
+
+print(id(a1))
+print(id(a2))
+print(id(a3))
 
 
-a1 = TYPE(a1)
 
-a2 = TYPE(a2)
+a4 = [1, 'aaa', 2]
+a5 = [1, 'aaa', 2]
 
 a1 == a2
 
@@ -23,3 +30,9 @@ True
 a1 is a2
 
 False
+
+print(a4)
+print(a5)
+
+print(id(a4))
+print(id(a5))
