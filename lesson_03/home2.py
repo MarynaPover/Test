0d825e3b5c012cad2473a27b1b11b6cccb06554b
MarynaPover <marynapover@gmail.com>
2022-12-21 19:54:26 +0000

    1

--- lesson_03/home2.py
+++ lesson_03/home2.py
@@ -1,9 +1,21 @@
+str_x = input()
 
+print(str_x)
+print(str_x[1::2])
+str_x1 = str_x[::-1]
+print(str_x1.upper())
 
+str_1 = 'абвге'
 
-str_1 = 'фаиррер'
-
+print(str_1)
 print(str_1[1::2])
-print(str_1[::-1])
+str_11 = str_1[::-1]
+print(str_11.upper())
 
-print(str_1.upper())
+str_2 = 'прст12'
+
+print(str_2)
+print(str_2[1::2])
+str_12 = str_2[::-1]
+print(str_12.upper())
+
