764e8ad4476a9766b9c9bfb86228717fd4bf2778
MarynaPover <marynapover@gmail.com>
2022-12-25 15:35:09 +0000

    1

--- lesson_4/home3.py
+++ lesson_4/home3.py
@@ -7,10 +7,20 @@ word_2 = sentence_1[1]
 print(word_1)
 print(word_2)
 
-x_1 = ('%s, %s' % (word_1, word_2))
-x_2 = ('{}, {}'.format(word_1, word_2))
-x_3 = (f', {word_1} {word_2}')
+word_1 = word_1[::-1]
+word_2 = word_2[::-1]
+word_1 = word_1.upper()
+word_2 = word_2.title()
 
+print(word_1)
+print(word_2)
 
+x_1 = ('%s %s' % (word_1, word_2))
+x_2 = ('{} {}'.format(word_1, word_2))
+x_3 = (f'{word_1} {word_2}')
+
+print(x_1)
+print(x_2)
+print(x_3)
 
 sourceFile = open('python4.txt', 'w')
