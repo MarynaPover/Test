33ea3ee0885dca9ac40f71f2cd3d2c5d24464b2e
MarynaPover <marynapover@gmail.com>
2022-12-25 14:35:33 +0000

    1

--- lesson_4/home3.py
+++ lesson_4/home3.py
@@ -1,5 +1,11 @@
 word_1= input('Введите первое слово:')
 word_2 = input('Введите второе слово:')
 
-print('%s, %s' % (word_1, word_2))
-print('{}, {}'.format(word_1, word_2))
+
+x_1 = ('%s, %s' % (word_1, word_2))
+x_2 = ('{}, {}'.format(word_1, word_2))
+x_3 =
+
+print(x_1)
+print(x_2)
+print(x_3)
