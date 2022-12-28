564ccbe8f6b34ad705a9c0dbab798ae72e7b72f8
MarynaPover <marynapover@gmail.com>
2022-12-28 18:31:12 +0000

    1

--- lesson_05/home4.py
+++ lesson_05/home4.py
@@ -3,9 +3,9 @@ age = input('Введите ваш возраст: ')
 
 if not age.isdigit() or int(age) <= 0:
     print('Ошибка, повторите ввод')
-elif int(age) <= 12:
-    print('Orange')
-elif 12 < int(age) < 18:
-    print('CocaCola')
+elif int(age) < 10:
+    print(f'Привет, шкет {name}')
+elif 10 <= int(age) <= 18:
+    print(f'Как жизнь {name}?')
 else:
     print('Beer')
