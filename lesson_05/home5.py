fd7b06f49bfaadde5e9ce8174d0966e3f7b1b582
MarynaPover <marynapover@gmail.com>
2022-12-28 19:33:22 +0000

    1

--- lesson_05/home5.py
+++ lesson_05/home5.py
@@ -3,8 +3,9 @@ n = input("Число: ")
 if not n.isdigit() or int(n) <= 0:
     print('Wrong input')
 else:
-    while int(n) > 0:
-        if n % 3 != 0:
+    n = int(n)
+    while n > 0:
+        if  % 3 != 0:
             result += n**3
     print('n:', n)
     print('result:', result)
