630adc96866a71f39b87e7cd806774cde3e556f7
MarynaPover <marynapover@gmail.com>
2022-12-28 19:18:33 +0000

    1

--- lesson_05/home5.py
+++ lesson_05/home5.py
@@ -1,8 +1,9 @@
-q = 1
 result = 0
 n = int(input("Число: "))
 while n > 0:
     if n % 3 != 0:
         result += n**3
+    print('n:', n)
+    print('result:', result)
     n -= 1
 print(f'general result: {result}')
