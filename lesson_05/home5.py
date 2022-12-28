d208d4a58e265e21ba07a5849aada88803a5adc5
MarynaPover <marynapover@gmail.com>
2022-12-28 19:45:30 +0000

    1

--- lesson_05/home5.py
+++ lesson_05/home5.py
@@ -1,13 +1,7 @@
 result = 0
-n = input("Число: ")
-if not n.isdigit() or int(n) <= 0:
-    print('Wrong input')
-else:
-    n = int(n)
-    print('n:', n)
-    while n > 0:
-        if n % 3 != 0:
-            result += n**3
-            print('result:', result)
+n = int(input("Введите число: "))
+while n > 0:
+    if n % 3 != 0:
+        result += n ** 3
     n -= 1
 print(f'general result: {result}')
