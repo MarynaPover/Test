2a3a51092dd26ac1e607c3f8de20e8905dc28427
MarynaPover <marynapover@gmail.com>
2022-12-28 18:26:47 +0000

    1

--- lesson_05/home4.py
+++ lesson_05/home4.py
@@ -1,8 +1,8 @@
-name = input('Enter your name: ')
-age = input('Enter your age: ')
+name = input('Введите ваше имя: ')
+age = input('Введите ваш возраст: ')
 
 if not age.isdigit() or int(age) == 0:
-    print('Wrong input')
+    print('Ошибка, повторите ввод')
 elif int(age) <= 12:
     print('Orange')
 elif 12 < int(age) < 18:
