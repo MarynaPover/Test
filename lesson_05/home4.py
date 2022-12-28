0928ead6c97dc9ddb6472cce2fe1ea23dc5e5b23
MarynaPover <marynapover@gmail.com>
2022-12-28 18:34:59 +0000

    1

--- lesson_05/home4.py
+++ lesson_05/home4.py
@@ -1,13 +1,13 @@
-name = input('Введите ваше имя: ')
-age = input('Введите ваш возраст: ')
-
-if not age.isdigit() or int(age) <= 0:
-    print('Ошибка, повторите ввод')
-elif int(age) < 10:
-    print(f'Привет, шкет {name}')
-elif 10 <= int(age) <= 18:
-    print(f'Как жизнь {name}?')
-elif 18 < int(age) < 100:
-    print(f'Что желаете {name}?')
-else:
-    print(f'{name}, вы лжете - в наше время столько не живут...')
+while True:
+    name = input('Введите ваше имя: ')
+    age = input('Введите ваш возраст: ')
+    if not age.isdigit() or int(age) <= 0:
+        print('Ошибка, повторите ввод')
+    elif int(age) < 10:
+        print(f'Привет, шкет {name}')
+    elif 10 <= int(age) <= 18:
+        print(f'Как жизнь {name}?')
+    elif 18 < int(age) < 100:\
+        print(f'Что желаете {name}?')
+    else:
+        print(f'{name}, вы лжете - в наше время столько не живут...')
