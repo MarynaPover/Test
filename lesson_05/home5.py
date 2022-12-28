e2e82bc4c8440353130ed9ef2800740bb6f4a89d
MarynaPover <marynapover@gmail.com>
2022-12-28 19:39:26 +0000

    1

--- lesson_05/home5.py
+++ lesson_05/home5.py
@@ -4,10 +4,11 @@ if not n.isdigit() or int(n) <= 0:
     print('Wrong input')
 else:
     n = int(n)
-    while n > 0:
-        if % 3 != 0:
-            result += n ** 3
     print('n:', n)
+    while n > 0:
+        if n % 3 != 0:
+            result += n ** 3
+
     print('result:', result)
     n -= 1
 print(f'general result: {result}')
