731ab4a7533026ba8deb2ac0589e34119eb3ee62
MarynaPover <marynapover@gmail.com>
2023-01-18 17:03:58 +0000

    1

---
+++ lesson_09/home12.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+def decorator(func):
+    def wrapper(*args, **kvargs):
+        startTime = datetime.now()
+        result = func(*args, **kvargs)
+        print("Затраченое время:", datetime.now() - startTime)
+        return result
+    return wrapper
+
+@decorator
+def CountFrequency(my_list):
+    count = {}
+    for i in my_list:
+        count[i] = count.get(i, 0) + 1
+    return count
+my_list = [6, 6, 1, 2, 2, 3, 2, 6, 6, 9, 8, 4, 4, 2, 3, 1, 2]
+print(CountFrequency(my_list))
+
