f3785eb909ef8c65e775683a13d3709c7ccd422d
MarynaPover <marynapover@gmail.com>
2023-01-19 19:06:16 +0000

    1

--- lesson_11/home17.py
+++ lesson_11/home17.py
@@ -1,6 +1,7 @@
 x = b'r\xc3\xa9sum\xc3\xa9'
-print(x.decode())
 x_1 = x.decode()
-print(x_1.encode('Latin1'))
+print(x_1)
 x_2 = x_1.encode('Latin1')
-print(x_2.decode('Latin1'))
+print(x_2)
+x_3 = x_2.decode('Latin1')
+print(x_3)
