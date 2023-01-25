7fd699603400e69901a87e1e8f89975b09b1ffe1
MarynaPover <marynapover@gmail.com>
2023-01-25 11:20:15 +0000

    1

--- lesson_12/home19.py
+++ lesson_12/home19.py
@@ -1,6 +1,6 @@
 import json
 
-input_data = {000001: ('Jason', 75), 000002: ('Sarah', 22), 000003: ('Ben', 13), 000004: ('Paul', 26), 000005: ('Mino', 46), 000006: ('Lilly', 8)}
+input_data = {100001: ('Jason', 75), 100002: ('Sarah', 22), 100003: ('Ben', 13), 100004: ('Paul', 26), 100005: ('Mino', 46), 100006: ('Lilly', 8)}
 
 with open('data_file.json', 'w') as f:
     json.dump(input_data, f)
